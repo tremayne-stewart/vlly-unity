ece94e9ac1c6d6fff5abf9c0fdf79f45da92f2ba
Tremayne Stewart <tremayne.u.stewart@gmail.com>
2023-09-07 13:13:53 -0400

    Change defaults to match firebase

--- Vlly/Config.cs
+++ Vlly/Config.cs
@@ -1,13 +1,13 @@
 namespace vlly {
   internal static class Config {
     // Can be overriden by VllySettings
-    internal static string APIHostAddress = "";
+    internal static string APIHostAddress = "https://us-central1-vlly-unity.cloudfunctions.net/";
     internal static string APIKey = "";
     internal static bool ShowDebug = false;
     internal static bool ManualInitialization = false;
 
-    internal static float FlushInterval = 5f;
-    internal static int FramesPerBatch = 12;
+    internal static float FlushInterval = 3f;
+    internal static int FramesPerBatch = 3;
     internal static float RecordingFPS = 24f;
   }
 } 
